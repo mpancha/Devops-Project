213516acf8b21fb4df120db4cc44ba863aedd6ce
Mitul Panchal <mpancha@ncsu.edu>
2015-11-17 15:25:15 -0500

    infrastructure

--- deployment/main.js
+++ deployment/main.js
@@ -2,40 +2,74 @@
 var express = require('express')
 var redis = require('redis')
 var bodyParser = require('body-parser')
-var app = express()
-var lb = express()
+var mailin = require('mailin');
 var Ansible = require('node-ansible')
-var reload_production = new Ansible.Playbook().playbook('production').inventory('inventory_production');
-var reload_canary = new Ansible.Playbook().playbook('canary').inventory('inventory_canary');
-var client = redis.createClient(6379, '127.0.0.1', {});
-//var prod = reload_production.exec();
-//var can = reload_canary.exec();
-//can.then(function(successResult) {
-//  console.log(successResult.code); // Exit code of the executed command 
-//  console.log(successResult.output) // Standard output/error of the executed command 
-//}, function(error) {
-//  console.error(error);
-//})
-//prod.then(function(successResult) {
-//  console.log(successResult.code); // Exit code of the executed command 
-//  console.log(successResult.output) // Standard output/error of the executed command 
-//}, function(error) {
-//  console.error(error);
-//})
-// Process POST from webhooks to deploy to production
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.post('/deploy', function(req, res){
-   console.log(req.body.ref) // form fields
-   if(req.body.ref.indexOf('dev') > -1) {
-       var deploy = reload_production.exec();
-       deploy.then(function(success){console.log(success.output)}, function(error){console.error(error);})
-   };
-   if(req.body.ref.indexOf('release') > -1) {
-       var deploy = reload_canary.exec();
-       deploy.then(function(success){console.log(success.output)}, function(error){console.error(error);})
-   };
-   res.status(204).end()
-});
-console.log("Starting App at 8000")
-app.listen(8000) 
+var http      = require('http');
+var httpProxy = require('http-proxy');
+
+var infrastructure = {
+  var proxyserver, infra;
+  setup: function(){
+     var CANARY = client.get('canary');
+     var PRODUCTION = client.get('production');
+     var count = 0;
+     var app = express()
+     var reload_production = new Ansible.Playbook().playbook('production').inventory('inventory_production');
+     var reload_canary = new Ansible.Playbook().playbook('canary').inventory('inventory_canary');
+     var client = redis.createClient(6379, '127.0.0.1', {});
+     var prod_only = false;
+
+     // Process POST from webhooks to deploy to production
+     app.use(bodyParser.json())
+     app.use(bodyParser.urlencoded({ extended: true }))
+     app.post('/deploy', function(req, res){
+        console.log(req.body.ref) // form fields
+        if(req.body.ref.indexOf('dev') > -1) {
+           var deploy = reload_production.exec();
+           deploy.then(function(success){console.log(success.output)}, function(error){console.error(error);})
+        };
+        if(req.body.ref.indexOf('release') > -1) {
+           var deploy = reload_canary.exec();
+           deploy.then(function(success){console.log(success.output)}, function(error){console.error(error);})
+        };
+        res.status(204).end()
+     });
+    
+     // start alert reciever
+     mailin.start({
+        port: 25,
+        disableWebhook: true // Disable the webhook posting.
+     });
+
+    /* Event emitted after a message was received and parsed. */
+     mailin.on('message', function (connection, data, content) {
+        console.log(data);
+        console.log('Stopping canary');
+        prod_only = true;
+     });
+
+     proxyserver  = http.createServer(function(req, res) {
+       count++;
+       if(prod_only == false && count % 10 == 1){
+          proxy.web( req, res, {target: CANARY } );
+       } else {
+          proxy.web( req, res, {target: PRODUCTION } );
+       }
+     }).listen(8080);
+     infra = app.listen(8000);
+  }
+  teardown: function() {
+     proxyserver.close();
+     infra.close();
+     exec('./configure.py clean', function(){
+         console.log("Infrastructure shutdown\n");
+     }
+  }
+}
+infrastructure.setup();
+// Clean Up
+process.on('exit', function(){infrastructure.teardown();} );
+process.on('SIGINT', function(){infrastructure.teardown();} );
+process.on('uncaughtException', function(err){
+  console.error(err);
+  infrastructure.teardown();} );
