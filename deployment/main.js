ca5a56b0c64ceeaf52af1d817d9924c031c9e60a
Mitul PAnchal <mpancha@ncsu.edu>
2015-11-17 21:05:33 +0000

    mailin

--- deployment/main.js
+++ deployment/main.js
@@ -7,16 +7,16 @@ var Ansible = require('node-ansible')
 var http      = require('http');
 var httpProxy = require('http-proxy');
 
+var pserver,infra;
 var infrastructure = {
-  var proxyserver, infra;
   setup: function(){
+     var client = redis.createClient(6379, '127.0.0.1', {});
      var CANARY = client.get('canary');
      var PRODUCTION = client.get('production');
      var count = 0;
      var app = express()
      var reload_production = new Ansible.Playbook().playbook('production').inventory('inventory_production');
      var reload_canary = new Ansible.Playbook().playbook('canary').inventory('inventory_canary');
-     var client = redis.createClient(6379, '127.0.0.1', {});
      var prod_only = false;
 
      // Process POST from webhooks to deploy to production
@@ -40,6 +40,26 @@ var infrastructure = {
         port: 25,
         disableWebhook: true // Disable the webhook posting.
      });
+    /* Access simplesmtp server instance. */
+mailin.on('authorizeUser', function(connection, username, password, done) {
+  if (username == "johnsmith" && password == "mysecret") {
+    done(null, true);
+  } else {
+    done(new Error("Unauthorized!"), false);
+  }
+});
+
+/* Event emitted when a connection with the Mailin smtp server is initiated. */
+mailin.on('startMessage', function (connection) {
+  /* connection = {
+      from: 'sender@somedomain.com',
+      to: 'someaddress@yourdomain.com',
+      id: 't84h5ugf',
+      authentication: { username: null, authenticated: false, status: 'NORMAL' }
+    }
+  }; */
+  console.log(connection);
+});
 
     /* Event emitted after a message was received and parsed. */
      mailin.on('message', function (connection, data, content) {
@@ -48,28 +68,29 @@ var infrastructure = {
         prod_only = true;
      });
 
-     proxyserver  = http.createServer(function(req, res) {
+     pserver  = http.createServer(function(req, res) {
        count++;
        if(prod_only == false && count % 10 == 1){
           proxy.web( req, res, {target: CANARY } );
        } else {
           proxy.web( req, res, {target: PRODUCTION } );
        }
-     }).listen(8080);
+     }).listen(3000);
      infra = app.listen(8000);
-  }
+  },
+
   teardown: function() {
-     proxyserver.close();
+     pserver.close();
      infra.close();
-     exec('./configure.py clean', function(){
-         console.log("Infrastructure shutdown\n");
-     }
-  }
+     //exec('./configure.py clean', function(){
+     //    console.log("Infrastructure shutdown\n");
+     // }
+  },
 }
 infrastructure.setup();
 // Clean Up
-process.on('exit', function(){infrastructure.teardown();} );
-process.on('SIGINT', function(){infrastructure.teardown();} );
-process.on('uncaughtException', function(err){
-  console.error(err);
-  infrastructure.teardown();} );
+//process.on('exit', function(){infrastructure.teardown();} );
+//process.on('SIGINT', function(){infrastructure.teardown();} );
+//process.on('uncaughtException', function(err){
+//  console.error(err);
+//  infrastructure.teardown();} );
